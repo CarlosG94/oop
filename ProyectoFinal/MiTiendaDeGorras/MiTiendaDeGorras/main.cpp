ff6ba58ebf9ca7236756781ae94b8d8531cc9ed2
CarlosG94 <carlos.gtz.94@gmail.com>
2015-05-13 22:11:41 -0500

    Adding final project

---
+++ ProyectoFinal/MiTiendaDeGorras/MiTiendaDeGorras/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MiTiendaDeGorras
+//
+//  Created by CarlosGutierrez on 5/7/15.
+//  Copyright (c) 2015 Carlos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
